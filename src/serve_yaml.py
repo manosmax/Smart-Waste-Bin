3f3c2f33e5de209d100b6c45255e8c5fefa2012d
manosmax <manolhs.pasamixalhs@gmail.com>
2026-05-25 14:21:53 +0300

    Update serve_yaml.py

--- src/serve_yaml.py
+++ src/serve_yaml.py
@@ -4,6 +4,7 @@ class YAMLHandler(http.server.SimpleHTTPRequestHandler):
     def end_headers(self):
         if self.path.endswith('.yml'):
             self.send_header('Content-Type', 'text/plain; charset=utf-8')
+            self.send_header('X-Content-Type-Options', 'nosniff')
         super().end_headers()
 
 def main():
