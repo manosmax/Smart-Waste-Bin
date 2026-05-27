a04a54cb10c967906111c3ac5aaa1800e25d298a
Anastasis Kanellopoulos <anastasisknl@gmail.com>
2026-05-27 21:51:12 +0300

    removed event based sampler

--- src/pirlib/sampler.py
+++ src/pirlib/sampler.py
@@ -6,21 +6,18 @@ except Exception:
     _GPIO_AVAILABLE = False
 
 class PirSampler:
-    def __init__(self, pin: int, callback, debounce_ms: int = 300):
+    def __init__(self, pin: int):
         self.pin = pin
         self._stub = not _GPIO_AVAILABLE
         if not self._stub:
             GPIO.setmode(GPIO.BCM)
-            GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-            # Callback μόνο σε RISING edge, με debounce 300ms
-            GPIO.add_event_detect(
-                pin,
-                GPIO.RISING,
-                callback=callback,
-                bouncetime=debounce_ms
-            )
+            GPIO.setup(pin, GPIO.IN)
+
+    def read(self) -> bool:
+        if self._stub:
+            return False
+        return bool(GPIO.input(self.pin))
 
     def cleanup(self):
         if not self._stub:
-            GPIO.remove_event_detect(self.pin)
             GPIO.cleanup(self.pin)
